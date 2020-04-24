96fc63e00ec2a90a92b14480e41f5d4d76b1408a
svd09 <svd14@case.edu>
2020-04-24 18:17:10 -0400

    some more functions added

---
+++ R/proc_kmcurve1.R
@@ -0,0 +1,30 @@
+#' @title A function to create personalized KM curve for 1 group
+#' @author Salil Deo
+#' @description Provides KM curve for 1 group with CI band
+#' @param s survfit object
+#' @param xlab x-axis label
+#' @param ylab y-axis label
+#' @param color string providing color label
+#' @seealso \code{\link[survival]{survfit}}
+#' @return NULL
+#' @examples \dontrun{
+#' # do not run this
+#' library(survival)
+#' s = survfit(Surv(time, status) ~ 1, data = lung)
+#' figure <- proc_kmcurve2(s = s, xlab = "follow-up", ylab = "proportion surviving",
+#' color = "blue")
+#' gets an object figure is a ggplot2 object; it can be further modified if needed.
+#' }
+
+proc_kmcurve1 <- function(s,xlab,ylab,color){
+  df <- tidy(s) # get the tidy summary suvfit object
+  finalt <- max(df$time) # maximum time set for graph
+  # start preparing the graph
+
+  g <- ggplot(data = df, aes(x = time, y = estimate)) +
+    geom_line() +
+    geom_ribbon(data = df, aes(ymax = conf.high, ymin = conf.low), alpha = 0.2,fill = color)
+  g2 <- g + xlab(xlab) + ylab(ylab) + ylim(0,1)
+  g3 <- g2 + my_theme()
+  g3
+}
